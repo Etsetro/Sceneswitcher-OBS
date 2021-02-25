52fe99b8cdf7739146098a0fcc3207a43f0f74f3
Etsetro <ronalds.palacis@outlook.com>
2021-02-25 23:53:16 +0200

    User interaction handlers

--- public/js/audioClient.js
+++ public/js/audioClient.js
@@ -33,7 +33,26 @@ function addAudioStream(stream) {
     let values = arr.reduce((a, b) => a + b, 0);
     visualizer.innerHTML = Math.round(values / 1000);
   };
-
-    visualizer.innerHTML = Math.round(values / 100);
-  };
 }
+
+function handleSlider(index, val) {
+  const output = document.querySelector(`.volume-output-${index}`);
+  output.innerHTML = val;
+}
+
+function handleInput(val) {}
+
+function handleSubmit() {}
+
+window.onload = () => {
+  for (let i = 0; i < 4; i++) {
+    document.querySelector(".scenes").innerHTML += `
+      <div>
+          <h3>Scene name</h3>
+          <input type="text" class="scene-name-${i}" onchange="handleInput(${i}, this.value)">
+          <input type=range class="volume-slider" min=0 max=100 value=1 step=1 oninput="handleSlider(${i}, this.value)">
+          <h2 class="volume-output-${i}">1</h2>
+      </div>
+    `;
+  }
+};
